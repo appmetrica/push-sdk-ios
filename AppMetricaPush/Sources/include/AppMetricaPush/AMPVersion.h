a4961376e15c930a9f0dcaccecedc3d88fb1f478
alex-oleynik <alex-oleynik@yandex-team.com>
2025-02-26 10:12:55 +0300

    AppMetrica Push version up 3.0.2 commit_hash:50341d8856c5bb6fd9136a403d7a513ccd6821e7

--- AppMetricaPush/Sources/include/AppMetricaPush/AMPVersion.h
+++ AppMetricaPush/Sources/include/AppMetricaPush/AMPVersion.h
@@ -4,7 +4,7 @@
 
 #define AMP_VERSION_MAJOR 3
 #define AMP_VERSION_MINOR 0
-#define AMP_VERSION_PATCH 1
+#define AMP_VERSION_PATCH 2
 
 // This line is uncommented in pre-releases.
 // #define AMP_VERSION_PRERELEASE_ID "rc.1"
--- AppMetricaPushLazy.podspec
+++ AppMetricaPushLazy.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name = 'AppMetricaPushLazy'
-  s.version = '3.0.1'
+  s.version = '3.0.2'
   s.summary = 'AppMetrica Push Lazy Notifications SDK'
 
   s.homepage = 'https://appmetrica.io'
